477231520a2e5aec5c85ac42700348a864efc775
Kalvir Sandhu <kalv@kalv.co.uk>
2013-03-12 18:56:15 -0700

    Add some keyboard shortcut love, up, down, left, right

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,45 @@
+var Slides = {
+  currentSlide: 1,
+  totalSlides: undefined,
+  moveDown: function() {
+    newSlide = Slides.currentSlide + 1;
+    if (newSlide <= Slides.totalSlides) {
+      Slides.scrollTo(newSlide);
+    }
+  },
+  moveUp: function() {
+    newSlide = Slides.currentSlide - 1;
+    if (newSlide > 0) {
+      Slides.scrollTo(newSlide);
+    }
+  },
+  scrollTo: function(number) {
+    $.scrollTo("#" + number, 500, {easing: "easeInOutExpo"});
+    Slides.currentSlide = number;
+  }
+}
+
 $(document).ready(function() {
   $("#nav").scrollSpy();
 
+  Slides.totalSlides = $(".slide").length;
+
   $("#nav a").click(function(e) {
     e.preventDefault();
-    $.scrollTo($(this).attr("href"), 500, {easing: "easeInOutExpo"});
+    Slides.scrollTo(parseInt($(this).attr("href").replace("#","")));
   });
+
+  // Handle keyboard arrows
+  $(document).keydown(function(e){
+    if (e.keyCode == 37) { // left
+      Slides.moveUp();
+    } else if (e.keyCode == 39) { // right
+      Slides.moveDown();
+    } else if (e.keyCode == 38) { // up
+      Slides.moveUp();
+    } else if (e.keyCode == 40) { // down
+      Slides.moveDown();
+    }
+    return false;
+});
 });
